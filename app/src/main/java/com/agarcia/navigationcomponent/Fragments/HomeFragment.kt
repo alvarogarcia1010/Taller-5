5fd3c5ad1276cc3d81e69541179f27e45a332b80
alvarogarcia1010 <00111317@uca.edu.sv>
2019-06-06 15:21:32 -0600

    Fragments ready

---
+++ app/src/main/java/com/agarcia/navigationcomponent/Fragments/HomeFragment.kt
@@ -0,0 +1,18 @@
+package com.agarcia.navigationcomponent.Fragments
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.agarcia.navigationcomponent.R
+
+class HomeFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+    
+}
